fdb9dc2900ce947f9a9f0e81ac725efd84c28661
Mariia Yelisieieva <maria97.55ua@gmail.com>
2020-07-01 20:46:10 +0300

    migrations created

---
+++ BlognoteIdentityServer/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlognoteIdentityServer.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
--- BlognoteIdentityServer/Startup.cs
+++ BlognoteIdentityServer/Startup.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlognoteIdentityServer.Models;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BlognoteIdentityServer
 {
@@ -24,6 +31,36 @@ namespace BlognoteIdentityServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddIdentity<AppUser, IdentityRole>()
+              .AddEntityFrameworkStores<AppIdentityDbContext>()
+              .AddDefaultTokenProviders();
+
+            services.AddIdentityServer().AddDeveloperSigningCredential()
+               // this adds the operational data from DB (codes, tokens, consents)
+               .AddOperationalStore(options =>
+               {
+                   options.ConfigureDbContext = builder => builder.UseSqlServer(Configuration.GetConnectionString("Default"));
+                   // this enables automatic token cleanup. this is optional.
+                   options.EnableTokenCleanup = true;
+                   options.TokenCleanupInterval = 30; // interval in seconds
+               })
+               .AddInMemoryIdentityResources(Config.GetIdentityResources())
+               .AddInMemoryApiResources(Config.GetApiResources())
+               .AddInMemoryClients(Config.GetClients())
+               .AddAspNetIdentity<AppUser>();
+
+            //services.AddTransient<IProfileService, IdentityClaimsProfileService>();
+
+            //services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+            //   .AllowAnyMethod()
+            //   .AllowAnyHeader()));
+
+            //services.AddMvc(options =>
+            //{
+            //    options.EnableEndpointRouting = false;
+            //}).SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +82,7 @@ namespace BlognoteIdentityServer
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
@@ -52,6 +90,7 @@ namespace BlognoteIdentityServer
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
